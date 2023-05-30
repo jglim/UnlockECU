d334b3d58e443d1f05c59eae9982535c325f0b47
JinGen Lim <1116555+jglim@users.noreply.github.com>
2023-05-30 13:56:49 +0800

    Add SubaruSecurityAccess2018CY1 (77 definitions)

---
+++ UnlockECU/UnlockECU/Security/SubaruSecurityAccess2018CY1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace UnlockECU
+{
+    /// <summary>
+    /// Subaru SecurityAccess2018CY1 from SSM4
+    /// Seed is encrypted through AES128-ECB using a variant-specific key
+    /// Led and confirmed by @jnewb1 at https://github.com/jglim/UnlockECU/issues/25
+    /// </summary>
+    class SubaruSecurityAccess2018CY1 : SecurityProvider
+    {
+        public override bool GenerateKey(byte[] inSeed, byte[] outKey, int accessLevel, List<Parameter> parameters)
+        {
+            if ((inSeed.Length != 16) || (outKey.Length != 16))
+            {
+                return false;
+            }
+
+            using (AesManaged aes = new AesManaged())
+            {
+                aes.Mode = CipherMode.ECB;
+                aes.BlockSize = 128;
+                aes.KeySize = 128;
+                aes.Padding = PaddingMode.None;
+                aes.Key = GetParameterBytearray(parameters, "K");
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                encryptor.TransformBlock(inSeed, 0, inSeed.Length, outKey, 0);
+            }
+
+            return true;
+        }
+        public override string GetProviderName()
+        {
+            return "SubaruSecurityAccess2018CY1";
+        }
+    }
+}
