5cd097825cd0b1e78bc5077ef2648b515eaeb667
JinGen Lim <1116555+jglim@users.noreply.github.com>
2023-07-06 10:47:32 +0800

    Add IC907:61
    References https://github.com/jglim/UnlockECU/issues/27

---
+++ UnlockECU/UnlockECU/Security/ArrayReverseAlgo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnlockECU
+{
+    /// <summary>
+    /// Generic array reverse algo, where the seed/output are of equal length, and output is the reversed seed array
+    /// </summary>
+    class ArrayReverseAlgo : SecurityProvider
+    {
+        public override bool GenerateKey(byte[] inSeed, byte[] outKey, int accessLevel, List<Parameter> parameters)
+        {
+            if (inSeed.Length != outKey.Length)
+            {
+                return false;
+            }
+            Array.Reverse(inSeed);
+            Array.Copy(inSeed, outKey, inSeed.Length);
+            return true;
+        }
+        public override string GetProviderName()
+        {
+            return "ArrayReverseAlgo";
+        }
+    }
+}
+
